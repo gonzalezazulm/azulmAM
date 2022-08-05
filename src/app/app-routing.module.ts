5f7c3daa3b00e08393db379414e2733adee264b0
gonzalezazulm <azulgonzzalez@gmail.com>
2022-08-05 11:38:42 -0300

    cuarto commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'', component:HomeComponent},
-  {path:'contacto', component:ContactoComponent}
+  {path:'contacto', component:ContactoComponent},
+  {path:'servicios', component:ServiciosComponent},
+  {path:'tienda', component: TiendaComponent}
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { ComponentsModule } from '../components/components.module';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ContactoComponent
+    ContactoComponent,
+    ServiciosComponent,
+    TiendaComponent
   ],
   imports: [
     CommonModule,
